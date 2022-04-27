5d4e01bc45a499b72df7fbb354feb82ab4b04068
Gal Cohen <92460450+Gal25@users.noreply.github.com>
2022-04-27 11:50:10 +0300

    Update Captain.cpp

--- Captain.cpp
+++ Captain.cpp
@@ -1,13 +1,12 @@
 #include "Captain.hpp"
-// #include "Player.hpp"
 #include <stdexcept>
 #include <iostream>
 using namespace std;
 
 namespace coup {
     
-    Captain::Captain(Game game, string name){}
-    // void block(Player player){}
-    // void steal(Player player){}
+    Captain::Captain(Game const &game, string const &name){}
+    void Captain::block(Player const &player){}
+    void Captain::steal(Player const &player){}
 
 }
